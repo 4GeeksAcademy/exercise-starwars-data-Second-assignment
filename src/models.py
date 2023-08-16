40efd3d548304a679047c946c3e0abe2d159d803
gokaybalkaya <130371393+gokaybalkaya@users.noreply.github.com>
2023-08-16 18:33:40 +0000

    finished

--- src/models.py
+++ src/models.py
@@ -1,29 +1,46 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from datetime import datetime  
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime 
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password_hash = Column(String(250), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)  
+
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    description = Column(String(1000))
+    image_url = Column(String(500))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    description = Column(String(1000))
+    image_url = Column(String(500))
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'),nullable=False)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    created_at = Column(DateTime, default=datetime.utcnow)  # Import datetime.utcnow()
+
+    user = relationship(User)
+    character = relationship(Character)
+    planet = relationship(Planet)
 
     def to_dict(self):
         return {}
